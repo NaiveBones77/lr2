3d2d1ea5a9ac7da8ebb72d05e11e47f7ccde7515
Maxon <maxsonic99@gmail.com>
2021-12-13 23:30:50 +0300

    Добавлен метод test() для ИНС

--- lr2/INS.cpp
+++ lr2/INS.cpp
@@ -2,6 +2,8 @@
 #include <mutex>
 #include <WinSock2.h>
 #include <iostream>
+#include "protocol.h"
+#include <cstdlib>
 
 INS::INS(double Latitude, double Longitude, double H, double CourseTrue,
 	double Tungazh, double List, double VelocityNS, double VelocityEW, double VelocityVI,
@@ -21,9 +23,51 @@ INS::INS(double Latitude, double Longitude, double H, double CourseTrue,
 }
 
 void INS::test() {
+	auto start = std::chrono::system_clock::now();
+	double time_test = 5;
+
 	mutex.lock();
+	delete[] buffer;
+	char tmp[33];
+
+	DISCRETE pack;   // Слово состояния с признаками "Исправность ИНС" и "Нет начальных данных"
+
+	pack.value.address = 0270;
+	pack.value.preparation = 0;
+	pack.value.control = 0;
+	pack.value.navigation = 0;
+	pack.value.gyrocompas = 0;
+	pack.value.restart = 0;
+	pack.value.scale = 0;
+	pack.value.heating = 1;
+	pack.value.temp_control = 1;
+	pack.value.init_data = 0;		// "Нет начальных данных"
+	pack.value.no_reception = 0;
+	pack.value.INS = 1;				// "Исправность ИНС"
+	pack.value.acceleration = 0;
+	pack.value.readiness = 0;
+	pack.value.sm = 0;
+	pack.value.p = 0;
+
+	buffer = new char[sizeof(pack)];             
+	memset(buffer, 0, sizeof(pack));            
+	memcpy(buffer, &pack, sizeof(pack));     
+
+	auto end = std::chrono::system_clock::now();
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	while (elapsed_seconds.count() < time_test)
+	{
+		end = std::chrono::system_clock::now();
+		elapsed_seconds = end - start;
+	}
+
+	delete[] buffer;
+	buffer = new char[sizeof(pack)];            
+	memset(buffer, 0, sizeof(pack));            
+	memcpy(buffer, &pack, sizeof(pack));        
 
 	mutex.unlock();
+	std::cout << "test control end\n";
 }
 
 void INS::preparation() {
